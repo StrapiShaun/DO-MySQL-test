955f145d791e0f2cf24a45889a6ecad686817a6a
Shaun Brown <shaun.brown@strapi.io>
2022-05-20 17:10:40 +0200

    changed prod database.js

--- config/env/production/database.js
+++ config/env/production/database.js
@@ -1,23 +1,23 @@
-const parse = require("connection-string-parser").parse;
+// const parse = require("pg-connection-string").parse;
 
-const { host, port, database, user, password } = parse(
-  process.env.DATABASE_URL
-);
+// const { host, port, database, user, password } = parse(
+//   process.env.DATABASE_URL
+// );
 
 
-module.exports = ({ env }) => ({
-    connection: {
-      engine: 'mysql',
-      connection: {
-        host,
-        port,
-        database,
-        user,
-        password,
-        //ssl: 'required'
-      },
-        debug: false,
-    },
-  });
+// module.exports = ({ env }) => ({
+//     connection: {
+//       client: 'mysql',
+//       connection: {
+//         host,
+//         port,
+//         database,
+//         user,
+//         password,
+//         ssl: 'required'
+//       },
+//         debug: false,
+//     },
+//   });
 
-  
+module.exports = ({ env }) => ({ defaultConnection: 'default', connections: { default: { connector: 'bookshelf', settings: { client: 'mysql', host: env('DATABASE_HOST', 'localhost'), port: env.int('DATABASE_PORT', 3306), database: env('DATABASE_NAME', 'strapi'), username: env('DATABASE_USERNAME', 'strapi'), password: env('DATABASE_PASSWORD', 'strapi'), }, options: {}, }, }, });
