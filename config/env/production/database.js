82250be6d76e22047a67fb858b35e955f0a4cced
Shaun Brown <shaun.brown@strapi.io>
2022-06-02 16:11:17 +0200

    add connection

--- config/env/production/database.js
+++ config/env/production/database.js
@@ -4,15 +4,17 @@ const config =parse(process.env.DATABASE_URL);
 
 
 module.exports = ({ env }) => ({
-       client: 'mysql',
-      connection: {   
-           user: config.user,
-           password: config.password,
-           database: config.database,
-           host: config.host,
-           port: config.port,
+  connection:{     
+    client: 'mysql',
+    connection: {   
+      user: config.user,
+      password: config.password,
+      database: config.database,
+      host: config.host,
+      port: config.port,
            
     },
+  },
       debug: false,
 });
 
