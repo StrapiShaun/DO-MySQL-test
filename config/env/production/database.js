ab2ccae3ae6857fc6d9db105e4e518b33f5d3de7
Shaun Brown <shaun.brown@strapi.io>
2022-06-07 17:04:51 +0200

    test db config change name

--- config/env/production/database.js
+++ config/env/production/database.js
@@ -1,12 +1,12 @@
 const parse = require("pg-connection-string").parse;
-const knex = require('knex')({client: 'mysql'})
+const knex = require('knex')({client: 'MySQL'})
 const config =parse(process.env.DATABASE_URL);
 
 
 
 module.exports = ({ env }) => ({
   connection:{     
-    client: "mysql",
+    client: "MySQL",
     connection: {   
       user: config.user,
       password: config.password,
