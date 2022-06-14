5825e006bd38ae6a9ceaf31616782aeb6913a0b8
Shaun Brown <shaun.brown@strapi.io>
2022-06-14 05:34:07 +0200

    db config change

--- config/env/production/database.js
+++ config/env/production/database.js
@@ -1,30 +1,29 @@
-// const parse = require("pg-connection-string").parse;
+ const parse = require("pg-connection-string").parse;
 // const knex = require('knex')({client: 'mysql'});
-// //const config =parse(process.env.DATABASE_URL);
+const config =parse(process.env.DATABASE_URL);
 
-const knex = require('knex');
-knex.client = 'mysql';
+//const knex = require('knex');
+//knex.client = 'mysql';
 module.exports = ({ env }) => ({
  
-  
-  client: 'mysql',
-  connection: process.env.DATABASE_URL,
-  searchPath: 'knex,public',
-  pool: { min: 0, max: 7 }
+   mysql: 'mysql',
+  // connection: process.env.DATABASE_URL,
+  // searchPath: 'knex,public',
+  // pool: { min: 0, max: 7 }
    
 //   client: 'mysql',
-//   connection: {
-//     host,
-//     port,
-//     database,
-//     user,
-//     password,
-//     ssl: {
-//       rejectUnauthorized: false,
-//     },
+  connection: {
+    host,
+    port,
+    database,
+    user,
+    password,
+    ssl: {
+      rejectUnauthorized: false,
+    },
   
-//   debug: false,
-// },
+  debug: false,
+},
 
 });
 
